155c49eaefbaba2d97c57e4462ee8d60e8a97092
wangchao <magician.of.technique@gmail.com>
2020-01-17 22:14:20 +0800

    [*] article service 结构

--- blog-service/services/article-service.js
+++ blog-service/services/article-service.js
@@ -1,5 +1,26 @@
 const article = {
-      
+
+      /**
+       * 创建文章
+       */
+      create() {
+
+      },
+
+      /**
+       * 删除文章
+       */
+      deleteArticle() {
+
+      },
+
+      /**
+       * 更新文章
+       */
+      update() {
+
+      }
+
 }
 
 module.exports = article
