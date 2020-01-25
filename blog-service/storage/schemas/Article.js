92ab06a5e54a832d7649648f3eeafa9e62911533
wangchao <magician.of.technique@gmail.com>
2020-01-25 19:21:59 +0800

    [+] schema

---
+++ blog-service/storage/schemas/Article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+module.exports = new Schema({
+      title: String,
+      author: String,
+      category: String,
+      tag: [String],
+      date: { type: Date, default: Date.now },
+      summary: String,
+      body: String
+})
